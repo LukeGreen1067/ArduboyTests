5246a21de7c65819e002b25f7df13b73099fcdf2
Luke Green CS2019 <luke.green.2019@uni.strath.ac.uk>
2023-10-18 02:40:56 +0100

    Short session today tried to add tones and its playing a const tone need to work out why

--- buttonPress/buttonPress.ino
+++ buttonPress/buttonPress.ino
@@ -1,11 +1,23 @@
 #include <Arduboy2.h>
 
 Arduboy2 arduboy;
+BeepPin1 beep;
+
+constexpr uint8_t frameRate = 40;
+
+constexpr uint16_t toneLeft = beep.freq(200);
+constexpr uint16_t toneRight = beep.freq(261);
+constexpr uint16_t toneUp = beep.freq(523);
+constexpr uint16_t toneDown = beep.freq(175);
+constexpr uint16_t toneA = beep.freq(495);
+constexpr uint16_t toneB = beep.freq(550);
+
+constexpr uint8_t toneTime = 250 / (1000 / frameRate); // 10 being frame rate
 
 void setup() {
   arduboy.begin();
-
-  arduboy.setFrameRate(10);
+  beep.begin();
+  arduboy.setFrameRate(frameRate);
 }
 
 void loop() {
@@ -13,12 +25,19 @@ void loop() {
   if (!(arduboy.nextFrame()))
     return;
   
+  beep.timer();
+  
   arduboy.clear();
+
+  beep.noTone();
+
+  arduboy.pollButtons();
 
   arduboy.setCursor(4, 9);
 
   if(arduboy.pressed(LEFT_BUTTON))
     arduboy.print(F("You pressed the left button"));
+    playTone(toneLeft, toneTime);
   if(arduboy.pressed(RIGHT_BUTTON))
     arduboy.print(F("You pressed the right button"));
   if(arduboy.pressed(UP_BUTTON))
@@ -31,4 +50,9 @@ void loop() {
     arduboy.print(F("You pressed the b button"));
 
   arduboy.display();
+}
+
+void playTone(uint16_t count, uint8_t frames)
+{
+  beep.tone(count, frames);
 }
